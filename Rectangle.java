b61fd24daccd4b539a1345acec22c261d1a136c3
ameBreton <165066304+ameBreton@users.noreply.github.com>
2025-06-24 19:19:48 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -9,20 +9,46 @@ public class Rectangle{
 
   public double length(){
     // returns the size of the horizontal side of the rectangle
-    return A.x + B.x;
+    return Math.abs(A.x - B.x);
   }
 
   public double width(){
     // returns the size of the vertical side of the rectangle
-    return A.y + B.y;
+    return Math.abs(A.y - B.y);
   }
   
   public double perimeter(){
     // returns the perimeter of the rectangle
     return this.width()*2 + this.length()*2;
   }
+
+  public double area(){
+    return this.width()*this.length();
+  }
+  
   public boolean isSquare(){
     // returns whether or not the rectangle is a square
     return this.length() == this.width();
+  }
+
+  @Override
+  public boolean equals(Object obj){
+    if(this == obj){
+      return true;
+    }
+    if(obj == null){
+      return false;
+    }
+    if(obj.getClass() != this.getClass()){
+      return false;
+    }
+    Square other = (Square)obj;
+    if(this.length() != other.length()){
+      return false;
+    }
+    if(this.width() != other.width()){
+      return false;
+    }
+    return true;
   }
 }
