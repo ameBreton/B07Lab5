6a61260d293985c815dcf51353de11993bc9203a
ameBreton <165066304+ameBreton@users.noreply.github.com>
2025-06-24 19:11:57 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,13 +1,28 @@
 public class Rectangle{
-  Point A;
-  Point B;
+  Point A; // one vertex
+  Point B; // the other one
   
   public Rectangle(Point A, Point B){
     this.A = A;
     this.B = B;
   }
-  public double perimeter(){
-    
+
+  public double length(){
+    // returns the size of the horizontal side of the rectangle
+    return A.x + B.x;
+  }
+
+  public double width(){
+    // returns the size of the vertical side of the rectangle
+    return A.y + B.y;
   }
   
+  public double perimeter(){
+    // returns the perimeter of the rectangle
+    return this.width()*2 + this.length()*2;
+  }
+  public boolean isSquare(){
+    // returns whether or not the rectangle is a square
+    return this.length() == this.width();
+  }
 }
