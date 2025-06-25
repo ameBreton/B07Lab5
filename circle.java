5951e5445bc3eb4f510b032d3f7752d5c7388e5a
HoldWoodd <holdwoodd@gmail.com>
2025-06-25 00:33:27 -0400

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,43 @@
+public class Circle{
+  Point A;
+  double r;
+  
+  public Circle(Point A, double r){
+    this.A = A;
+    this.r = r;
+  }
+  
+  public Circle(Point A, Point B){
+    this.A = A;
+    this.r = A.distance(B); //A be center, B is a point on circle
+  }
+  
+  public double area(){
+    return this.r * this.r * Math.PI;
+  }
+  
+  public double perimeter(){
+    return this.r * 2 * Math.PI;
+  }
+
+  @Override
+  public boolean equals(Object obj){
+    if(this == obj){
+      return true;
+    }
+    if(obj == null){
+      return false;
+    }
+    if(obj.getClass() != this.getClass()){
+      return false;
+    }
+    Circle other = (Circle)obj;
+    if(!this.A.equals(other.A)){
+      return false;
+    }
+    if(this.r != other.r){
+      return false;
+    }
+    return true;
+  }
+}
