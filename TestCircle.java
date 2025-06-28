35590ce44a13dbeb0ceb00022ff37803ade4fa38
Apqrsmd272 <arno.peng@mail.utoronto.ca>
2025-06-28 16:49:49 -0400

    Update TestCircle.java

--- TestCircle.java
+++ TestCircle.java
@@ -1,64 +1,86 @@
+package lab4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class TestCircle{
 
-  @test
+  @Test
   void testarea(){
-    Point p1 = new Point(1.0，2.0);
+    Point p1 = new Point(1,2.0);
     Circle c1 = new Circle(p1,2);
     double area = c1.area();
-    assertequals(area, 4*math.PI);
+    assertEquals(area, 4* Math.PI);
   }
     
-  @test
-  void testperimeter{
-    Point p1 = new Point(1.0，2.0);
+  @Test
+  void testperimeter(){
+    Point p1 = new Point(1.0,2.0);
     Point p2 = new Point(3.0,2.0);
     Circle c1 = new Circle(p1,p2);//c1.r == 1?
     double perimeter = c1.perimeter();
-    assertequals(perimeter, 2* math.PI * 1);    
+    assertNotEquals(perimeter,2* Math.PI * 1);    
   }
 
-  @test
-  void testequals_A{
-    Point p1 = new Point(1.0，2.0);
-    Point p2 = new Point(2,0, 1.0);
+  @Test
+  void testequals_A(){
+    Point p1 = new Point(1.0,2.0);
+    Point p2 = new Point(2.0,1.0);
     Circle c1 = new Circle(p1,1);
     Circle c2 = new Circle(p2,1);
-    asserfalse(c1.equals(c2));//c1.A!=c2.A
+    assertFalse(c1.equals(c2));//c1.A!=c2.A
   }
-  @test
-  void testequals_B{
-    Point p1 = new Point(1.0，2.0);
+  @Test
+  void testequals_B(){
+    Point p1 = new Point(1.0,2.0);
     Point p2 = new Point(1.0, 2.0);
     Circle c1 = new Circle(p1,2);
     Circle c2 = new Circle(p2,1);
-    asserfalse(c1.equals(c2));//c1.r!=c2.r
+    assertFalse(c1.equals(c2));//c1.r!=c2.r
   }
 
-  @test
-  void testequals_C{
-    Point p1 = new Point(1.0，2.0);
+  @Test
+  void testequals_C(){
+	Point p1 = new Point(1.0,2.0);
     Circle c1 = new Circle(p1,2);
-    Circle c2 = c1
-    asserTrue(c1.equals(c2));//exactly the same
+    Circle c2 = c1;
+    assertTrue(c1.equals(c2));//exactly the same
   }
 
-  @test
-  void testequals_D{
-    Point p1 = new Point(1.0，2.0);
+  @Test
+  void testequals_F(){
+	Point p1 = new Point(1.0,2.0);
+	Point p2 = new Point(1,2);
     Circle c1 = new Circle(p1,2);
-    Circle c2 = null
-    asserfalse(c1.equals(c2));//null
+    Circle c2 = new Circle(p2,2);
+    assertTrue(c1.equals(c2));
   }
-
-  @test
-  void testequals_E{
-    Point p1 = new Point(1.0，2.0);
-    Circle c1 = new Circle(p1,2);
-    asserTrue(c1.equals(1));//point.getclass()!=int.getclass()
-  }
-
-
-
-
   
+  
+  
+  @Test
+  void testequals_D(){
+    Point p1 = new Point(1.0,2.0);
+    Circle c1 = new Circle(p1,2);
+    Circle c2 = null;
+    assertFalse(c1.equals(c2));//null
+  }
+
+  @Test
+  void testequals_E(){
+    Point p1 = new Point(1.0,2.0);
+    Circle c1 = new Circle(p1,2);
+    assertFalse(c1.equals(1));//point.getclass()!=int.getclass()
+  }
+
+  @Test
+  void testhashCode() {
+	  
+	  Point p1 = new Point(1.0,2.0);
+	  Point p2 = new Point(3,4);
+	  Circle c1 = new Circle(p1,2);
+	  Circle c2 = new Circle(p1,p2);
+	  assertNotEquals(c1.hashCode(), c2.hashCode());
+  } 
 }
